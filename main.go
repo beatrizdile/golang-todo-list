6d8474907c47782ce4b7a66b48be85226d3ce949
Beatriz Dile <bedos-sa@student.42sp.org.br>
2025-05-03 17:03:22 -0300

    feat: adding task item

--- main.go
+++ main.go
@@ -10,9 +10,24 @@ func main() {
 	var taskItems = []string{shortGolang, fullGolang, rewardDessert}
 
 	fmt.Println("##### Welcome to our Todolist App! #####")
-	fmt.Println("List of my Todos")
+	printTasks(taskItems)
+	fmt.Println()
 
+	taskItems = addTask(taskItems, "Go for a run")
+	taskItems = addTask(taskItems, "Practicing coding in Go")
+
+	fmt.Println("Updated List")
+	printTasks(taskItems)
+}
+
+func printTasks(taskItems []string) {
+	fmt.Println("List of my Todos")
 	for index, task := range taskItems {
-		fmt.Printf("Task #%d: %s\n", index, task)
+		fmt.Printf("Task #%d: %s\n", index+1, task)
 	}
+}
+
+func addTask(taskItems []string, newTask string) []string {
+	var updatedTaskItems = append(taskItems, newTask)
+	return updatedTaskItems
 }
